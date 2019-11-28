03af9052399bd5d4aa3fc8a0df8dcc6c850f095f
Barış Özbaş <baris.ozbas@ozu.edu.tr>
2019-11-28 15:33:35 +0300

    added dynamic home page and initial of item page

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-
+    <div class="container pt-5">
+        <div class="row">
+            <div class="col-6">
+                <img class="rounded-circle" style="height: 500px;width: auto" src="{{$item->photo}}">
     </div>
 @endsection
