651103af661e2e463861274648e5af8aab44754b
Kerem Güre <kerem.gure@ozu.edu.tr>
2019-11-30 10:47:08 +0300

    Added new features to item page and added category page.

---
+++ app/helpers.php
@@ -0,0 +1,15 @@
+<?php
+function createItemCard($item){
+    echo '
+        <div class="col-3">
+            <div class="card">
+                <img src="'.$item->photo.'" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">'.$item->item_name.'</h5>
+                    <p class="card-text">'.$item->item_desc.'</p>
+                    <a href="/item/'.$item->item_id.'" class="btn btn-primary">Go to Item Page</a>
+                </div>
+            </div>
+       </div>';
+};
+?>
--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -2,8 +2,22 @@
 
 @section('content')
     <div class="container pt-5">
+        <h1 class="text-center font-weight-bolder pb-2" style="color:#5d561b">{{$item->item_name}}</h1>
         <div class="row">
             <div class="col-6">
                 <img class="rounded-circle" style="height: 500px;width: auto" src="{{$item->photo}}">
+            </div>
+            <div class="col-6">
+                <div class="text-center">
+                    <h2 class="font-weight-bold pb-2" style="color:#2a9055;">&#x20BA {{$item->price}}</h2>
+                    @guest
+                        <buton class="btn btn-primary" onclick='alert("you need to sign in")'>Add to Basket</buton>
+                    @else
+                        <buton class="btn btn-primary" onclick='alert("added")'>Add to Basket</buton>
+                    @endguest
+                    <p class="pt-5 font-weight-bold" style="font-size: large">{{$item->item_desc}}</p>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
