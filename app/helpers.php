05f47e0b18ebf82bf8f0440e31ac6b6fa4f84ca7
Kerem Güre <kerem.gure@ozu.edu.tr>
2019-12-12 15:19:30 +0300

    added price to item cards.

--- app/helpers.php
+++ app/helpers.php
@@ -6,6 +6,9 @@ function createItemCard($item){
                 <img data-src="https://test.buildpc.software/photos/'.$item->photo.'" class="card-img-top lazy" alt=""> 
                 <div class="card-body">
                     <h5 class="card-title">'.$item->item_name.'</h5>
+                    <h5 class="card-subtitle py-2 font-weight-bolder" style="background: linear-gradient(to right, blueviolet , orange, orangered, red, darkred, indianred);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;">&#x20BA '.$item->price.'</h5>
                     <p class="card-text">'.$item->item_desc.'</p>
                     <a href="/item/'.$item->item_id.'" class="btn btn-primary">Go to Item Page</a>
                 </div>
