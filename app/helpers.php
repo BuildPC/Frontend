62d16fc04739eb44fb55ea480e16a0a65a0a2375
unknown <zined_burak@hotmail.com>
2019-12-13 09:54:21 +0300

    improved the view for shopping cart

--- app/helpers.php
+++ app/helpers.php
@@ -14,4 +14,26 @@ function createItemCard($item){
                 </div>
        </div>';
 };
+function createBasketItemCard($item){
+    // item özel düzenle
+    echo '
+    <tr>
+        <th scope="row" class="border-0">
+            <div class="p-2">
+                <img src="https://res.cloudinary.com/mhmd/image/upload/v1556670479/product-1_zrifhn.jpg" alt="" width="70" class="img-fluid rounded shadow-sm">
+                 <div class="ml-3 d-inline-block align-middle">
+                    <h5 class="mb-0"> <a href="#" class="text-dark d-inline-block align-middle">Timex Unisex Originals</a></h5><span class="text-muted font-weight-normal font-italic d-block">Category: Watches</span>
+                 </div>
+            </div>
+        </th>
+        <td class="border-0 align-middle"><strong>$79.00</strong></td>
+        <td class="border-0 align-middle"><strong>3</strong></td>
+        <td class="border-0 align-middle"><a href="#" class="text-dark"><i class="fa fa-trash"></i></a></td>
+     </tr>';
+
+
+
+
+
+}
 
